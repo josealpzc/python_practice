d3914a15eab29c931143c8aee181c20c3fb07365
Jose Lopez <j.antonio_lpz@hotmail.com>
2023-12-17 17:58:48 -0600

    Adding 'cout_cin_practice.cpp' and 'coutcinpractice' files

---
+++ cpp/cout_cin_practice.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	cout << "This is a message"; 
+	cout << "printed in the same line " <<endl; 
+	cout << "This is a msg" << endl;
+	cout << "printed in different" << endl;
+	cout << "lines" << endl;
+
+	int age = 28;
+	string name = "Jose";
+	cout << "Hello " << name << ", This is your current age" << age << endl;
+}
