51c1c48ef1d9b61bdfdf87addc64d3dc7348e4a4
Jose Lopez <j.antonio_lpz@hotmail.com>
2023-12-23 18:27:11 -0600

    Adding interactive menu and fixing bug on removeLast method (method did not worked when list had only one value).

--- python/linkedList.py
+++ python/linkedList.py
@@ -27,25 +27,30 @@ class LinkedList:
             self.head = Node(value)
 
         self.size+=1
-    #this methos may change, current implementation goes trough all the linked list to find out 
+    #this methods may change, current implementation goes trough all the linked list to find out 
     #who is the last Node, this migt cause a problem if the size of the list increases.
     #To fix this, it is necesary to create a 'last' variable that points to the last Node of the list.
     def removeLast(self):
         if(self.head):
-            current = self.head
-            
-            while(current.next !=None):
-                previous = current
-                current = current.next
-                 
-            previous.next = None
+            print(f"head: {self.head.value}")
+            if(self.head.next == None):
+                print ('yes None')
+                self.head = None
+                print (f'head: {self.head} DONE')
+            else:
+                current = self.head
+                while(current.next):
+                    previous = current
+                    current = current.next
+                previous.next=None
+
             self.size-=1
-            
+
         else:
             print("You can not remove from an empty list")
 
     def insertAt(self,value,pos):
-        
+
         if(pos>=1 and pos<=self.size+1):
             if(self.head):
                 if(pos==1):
@@ -72,7 +77,8 @@ class LinkedList:
                 self.head = Node(value)
             self.size+=1
         else:
-            print(f"Please select a number between 1 and {self.size}")
+            tmp = self.size+1
+            print(f"Please select a number between 1 and {tmp}")
 
     def insertLast(self,value):
         if (self.head):
@@ -94,13 +100,13 @@ class LinkedList:
                 self.head=None
             else:
                 self.head=self.head.next
-            print(f"Value removed: {self.head.value}")
             self.size-=1
         else:
             print("You can't remove from an empty list")
         
     def printSize(self):
         print(f"size of the list is: {self.size}")
+
     
     def printList(self):
 
@@ -112,30 +118,51 @@ class LinkedList:
                 current = current.next
             print(nodes)
         else:
-            print("List is empty")
+            print("[]")
+
 def main():
     #Create a list with 5 different values
     #  5->9->7->10->2
-    mylist = LinkedList()
-    for i in range(1,6):
-        mylist.insertLast(i)
 
-    mylist.printList()
-    mylist.printSize()
-    
-    mylist.insertAt(20,10)
-    mylist.removeFirst()
-    mylist.printList()
-    
-    mylist.removeLast()
-    mylist.printList()
-    mylist.printSize()
-    '''
-    mylist.insertAt('HERE',9)
-    mylist.printList()
-    mylist.printSize()
-    '''
- 
-    
+    run = True 
+    mylist = LinkedList()
+
+    while(run):
+        
+        print('List:')
+        mylist.printList()
+        print(f'Size: {mylist.size}')
+        response = int(input("""
+        Please select an action to perform:
+            1. Insert First
+            2. Insert Last
+            3. Remove First
+            4. Remove Last
+            5. Add At
+            6. Exit
+        """))                    
+        if(response == 1):
+            value = int(input("Enter a value:"))
+            mylist.insertFirst(value)
+
+        elif(response == 2):
+            value = int(input("Enter a value:"))
+            mylist.insertLast(value)
+
+        elif(response == 3):
+            mylist.removeFirst()
+
+        elif(response == 4):
+            mylist.removeLast()
+
+        elif(response == 5):
+            value = int(input("Enter your node value:"))
+            position = int(input(f"Enter a position value:"))
+            mylist.insertAt(value,position)
+        
+        elif(response == 6):
+            run=False
+        else:
+            print('choose a valid number')
 if __name__ == '__main__':
     main()
