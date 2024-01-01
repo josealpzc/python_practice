a0aaa13cd96d199bbb512e03360fd7d5d413177a
Jose Lopez <j.antonio_lpz@hotmail.com>
2023-12-31 19:20:49 -0600

    Adding recursive functions

--- python/recursion.py
+++ python/recursion.py
@@ -1,11 +1,50 @@
-#This method must print decreasing serie from a 'n' number given by the user to 0. 
-def recursive_count(n):        #Number is received by the function 
-    if(n < 0):                 #If n is less than 0 we should return 0
-        return 0               #return 0
-    else:                      #If n is still greather than 0 
-        print(n)               #print n
-        recursive_count(n-1)   #Use recursion to decrease n value
+"""
+Author: Jose Lopez
+Date: 12/31/2023
+Notes: This file contains some examples for practicing recursion.
 
-recursive_count(5) #Function receives a number given by the user
+"""
+#Print a decreasing count from 10 to 0 
+def decreasing_count(number):
+    
+    #This is the base condition: once the base condition is raised, the recursive method calls are completed, and
+    #the loop ends.
+    if(number < 0):
+        #Once 'number' is 0, the recursive calls to method end.
+        return 0 
+    
+    #If 'number' is greather than 0, we continue calling the method in a recursive way.
+    else:
+        print(number)
+        decreasing_count(number-1)
 
+#This method prints fibonnaci serie:
+def recursive_fibonacci(number):
+    """
+    #These are the starting numbers of a fibonacci serie. 
+    a=0
+    b=1
+    #This variable will be used to count from 0 to 'number'
 
+    if(number < 1):
+        return 0
+    else:
+        tmp=a+b
+        print(a)
+        a=b
+        b=tmp
+        recursive_fibonacci(number-1)
+    """
+def fibonacci(number):
+    a=0
+    b=1 
+    cnt=0
+
+    while(cnt<number):
+        tmp=a+b
+        print(a)
+        a=b
+        b=tmp
+        cnt+=1
+
+recursive_fibonacci(8)
