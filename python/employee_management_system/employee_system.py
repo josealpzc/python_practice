59929af0e0e731cc2f2a8808f34c34e9ed47bfbe
Jose Lopez <j.antonio_lpz@hotmail.com>
2024-01-13 21:30:24 -0600

    changing star_connection method to return connection object

--- python/employee_management_system/employee_system.py
+++ python/employee_management_system/employee_system.py
@@ -1,15 +1,22 @@
 import mysql.connector
 
 def start_connection(pswd):
+    return mysql.connector.connect(user='root',host='127.0.0.1',password=pswd,database='employees')
+    #print(cnx)
     
-    cnx = mysql.connector.connect(user='root',host='127.0.0.1',password=pswd,database='employees')
-    print(cnx)
-    cnx.close()
+#This function verifies if a given id corresponds to an employee.
+def check_employee(employee_id):
+    sql = 'select * from employee where id=%s'
+    #c=cnx.cursor
+    
 
 def main():
     print('Welcome')
     pswd = input('Please enter your password for sql: ')
-    start_connection(pswd)
+    cnx=start_connection(pswd)
+    #check_employee(cnx)
+    print(cnx)
+    cnx.close()
 
 if __name__ == '__main__':
     main()
