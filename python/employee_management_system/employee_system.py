228f4170dd0fef35ad61f03a1b6ad946c51e02d4
Jose Lopez <j.antonio_lpz@hotmail.com>
2024-01-13 21:05:27 -0600

    adding start_connection and main function

--- python/employee_management_system/employee_system.py
+++ python/employee_management_system/employee_system.py
@@ -1,5 +1,15 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(user='root', password='WWaterRule29.', host='localhost', database='employees')
+def start_connection(pswd):
+    
+    cnx = mysql.connector.connect(user='root',host='127.0.0.1',password=pswd,database='employees')
+    print(cnx)
+    cnx.close()
 
-cnx.close()
+def main():
+    print('Welcome')
+    pswd = input('Please enter your password for sql: ')
+    start_connection(pswd)
+
+if __name__ == '__main__':
+    main()
