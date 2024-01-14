e3b1f8a56b7f146c1db8abacb77b473e4525d636
Jose Lopez <j.antonio_lpz@hotmail.com>
2024-01-14 13:03:00 -0600

    Testing with different IDs

--- python/employee_management_system/employee_system.py
+++ python/employee_management_system/employee_system.py
@@ -1,20 +1,39 @@
 import mysql.connector
+from getpass import getpass
 
+#This functions starts the connection to mysql server
 def start_connection(pswd):
     return mysql.connector.connect(user='root',host='127.0.0.1',password=pswd,database='employees')
     #print(cnx)
     
 #This function verifies if a given id corresponds to an employee.
 def check_employee(employee_id):
-    sql = 'select * from employee where id=%s'
-    #c=cnx.cursor
-    
+   sql = 'select * from employee where id=%s'
 
 def main():
+    
     print('Welcome')
-    pswd = input('Please enter your password for sql: ')
-    cnx=start_connection(pswd)
-    #check_employee(cnx)
+    password = getpass()
+    
+    #cnx is the connection object.
+    cnx=start_connection(password)
+    
+    sql = 'select * from employee where id=%s'
+    c = cnx.cursor(buffered=True)
+    
+    for i in range (0,7):
+        data = (i,)
+        c.execute(sql,data)
+        r = c.rowcount
+
+        if r == 1:
+            flag = True
+
+        else:
+            flag = False
+
+        print(f"Flag: {flag}")
+
     print(cnx)
     cnx.close()
 
